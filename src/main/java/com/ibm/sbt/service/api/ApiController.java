2acab15ce117a67a304ccea093cbcbee1633a67a
mduarte <mariofduarte@gmail.com>
2013-08-06 21:44:01 +0100

    test

--- src/main/java/com/ibm/sbt/service/api/ApiController.java
+++ src/main/java/com/ibm/sbt/service/api/ApiController.java
@@ -1,9 +1,16 @@
 package com.ibm.sbt.service.api;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.ibm.sbt.model.User;
 
@@ -11,6 +18,8 @@ import com.ibm.sbt.model.User;
 @RequestMapping("api")
 public class ApiController {
 
+	protected final Log logger = LogFactory.getLog(getClass());
+	
 	@RequestMapping("user")
 	@ResponseBody
 	public User randomPerson() {
@@ -22,5 +31,36 @@ public class ApiController {
 	    User user = new User();
 	    user.setName(name);
 	    return user;
+	}
+	
+	@RequestMapping("test1/**")
+	public String test1() {
+		logger.error("TEST1: **");
+		return "";
+	}
+	
+	@RequestMapping("test2/{path:.*}")
+	public String test2(@PathVariable("path") String path) {
+		logger.error("TEST2: "+path);
+		return "";
+	}
+	
+	@RequestMapping("test3/**")
+	public String test3(HttpServletRequest request) {
+//		String path = (String)request.getAttribute( HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE );
+		logger.error("TEST3: "+extractPathFromPattern(request));
+		return "";
+	}
+	
+	private static String extractPathFromPattern(final HttpServletRequest request){
+	    String path = (String) request.getAttribute(
+	            HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+	    String bestMatchPattern = (String ) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+
+	    AntPathMatcher apm = new AntPathMatcher();
+	    String finalPath = apm.extractPathWithinPattern(bestMatchPattern, path);
+
+	    return finalPath;
+
 	}
 }
--- src/main/java/com/ibm/sbt/service/proxy/SmartCloudProxyController.java
+++ src/main/java/com/ibm/sbt/service/proxy/SmartCloudProxyController.java
@@ -1,13 +1,16 @@
 package com.ibm.sbt.service.proxy;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.ibm.sbt.model.User;
 
@@ -32,9 +35,10 @@ public class SmartCloudProxyController {
 	    return user;
 	}
 	
-	@RequestMapping(value="/{path}", method=RequestMethod.GET)
+	@RequestMapping(value="/**", method=RequestMethod.GET)
 	@ResponseBody
-	public String proxyRequest(@PathVariable("path") String path) throws Exception {
+	public String proxyRequest(HttpServletRequest request) throws Exception {
+		String path = extractPathFromPattern(request);
 		logger.info("Path: "+path);
 		return proxyService.get(path);
 	}
@@ -44,6 +48,17 @@ public class SmartCloudProxyController {
 	@ResponseBody
 	public String getToken() {
 		return proxyService.getToken();
+	}
+	
+	private static String extractPathFromPattern(final HttpServletRequest request){
+	    String path = (String) request.getAttribute(
+	            HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+	    String bestMatchPattern = (String ) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+
+	    AntPathMatcher apm = new AntPathMatcher();
+	    String finalPath = apm.extractPathWithinPattern(bestMatchPattern, path);
+
+	    return finalPath;
 	}
 	
 	public void setProxyService(ProxyService proxyService) {
