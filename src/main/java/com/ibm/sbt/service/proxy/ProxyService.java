0e58a8204d9bbe0bb27002cd2caa81e6e1b84a80
mduarte <mariofduarte@gmail.com>
2013-08-06 15:10:25 +0100

    test

--- src/main/java/com/ibm/sbt/service/proxy/ProxyService.java
+++ src/main/java/com/ibm/sbt/service/proxy/ProxyService.java
@@ -5,18 +5,23 @@ import org.springframework.web.client.RestOperations;
 
 public class ProxyService {
 	private String baseUrl;
-	private RestOperations proxyAppRestTemplate;
+	private RestOperations restTemplate;
 	
 	
 	public void setBaseUrl(String baseUrl) {
 		this.baseUrl = baseUrl;
 	}
 	
-	public void setProxyAppRestTemplate(OAuth2RestTemplate proxyAppRestTemplate) {
-		this.proxyAppRestTemplate = proxyAppRestTemplate;
+	public void setRestTemplate(OAuth2RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+	
+	public String get(String urlPath) throws Exception {
+		String url = baseUrl + urlPath;
+		return restTemplate.getForObject(url, String.class);
 	}
 	
 	public String getToken() {
-		return ((OAuth2RestTemplate)proxyAppRestTemplate).getAccessToken().getValue();
+		return ((OAuth2RestTemplate)restTemplate).getAccessToken().getValue();
 	}
 }
--- src/main/java/com/ibm/sbt/service/proxy/SmartCloudProxyController.java
+++ src/main/java/com/ibm/sbt/service/proxy/SmartCloudProxyController.java
@@ -2,7 +2,9 @@ package com.ibm.sbt.service.proxy;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ibm.sbt.model.User;
@@ -25,6 +27,13 @@ public class SmartCloudProxyController {
 	    user.setName(name);
 	    return user;
 	}
+	
+	@RequestMapping(value="/{path}", method=RequestMethod.GET)
+	@ResponseBody
+	public String proxyRequest(@PathVariable("path") String path) throws Exception {
+		return proxyService.get(path);
+	}
+	
 	
 	@RequestMapping("token")
 	@ResponseBody
